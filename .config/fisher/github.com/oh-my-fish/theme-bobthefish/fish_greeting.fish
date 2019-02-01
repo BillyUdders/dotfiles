ca434dedd4ae91a96c74d964caa5becf903f2b3f
Rhys Davies <rdavies@neighborly.com>
2019-01-31 20:04:13 -0800

    Removing the fisher stuff, can just rock a fish_file on bootstrap

--- .config/fisher/github.com/oh-my-fish/theme-bobthefish/fish_greeting.fish
+++
@@ -1,6 +0,0 @@
-function fish_greeting -d "What's up, fish?"
-    set_color $fish_color_autosuggestion
-    uname -nmsr
-    command -q -s uptime; and uptime
-    set_color normal
-end
--- .config/fisher/github.com/reitzig/sdkman-for-fish/functions/sdk.fish
+++
@@ -1,9 +0,0 @@
-# Guard: SDKMAN! needs to be installed
-if not test -f "$__fish_sdkman_init"
-  exit 0
-end
-
-# Declare the sdk command for fish
-function sdk -d "Manage SDKs"
-    __fish_sdkman_run_in_bash "source \"$__fish_sdkman_noexport_init\" && sdk $argv"
-end
